f61307e92f5640ffc2e3aeff37a4c459cdd88e0c
aca00 <akhilorappady@gmail.com>
2023-03-04 22:12:21 +0530

    Check if the url is valid (http or https)

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,6 @@
 // Send a message to the background script to get the active tab's URL.
-chrome.runtime.sendMessage('getTabUrl', (response) => {
+chrome.runtime.sendMessage('refresh', (response) => {
     // Update the text of a <p> element in the popup to show the URL.
     const urlText = document.getElementById('para');
-    urlText.textContent = response.url;
+    urlText.textContent = response.data;
 });
--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,29 @@
 try {
     importScripts('./dist/bundle.js');
     console.log("Loaded script successfully")
-    worker = new Worker.Worker();
+
+
+    let url = null;
+    let message = {};
+    const worker = new Worker.Worker();
     // worker.createNewCollection("a/b/c");
 
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        if (message === 'getTabUrl') {
+        if (message === 'refresh') {
             // Get the active tab in the current window.
             chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                // Send the URL to the popup.
-                sendResponse({ url: tabs[0].url });
+                url = tabs[0].url // string
+                if (url && url.match(new RegExp("http[s]?\:\/\/.*", "gi"))) {
+                    console.log("valid_url");
+                    url = new URL(url)
+                    var domain = url.hostname.split('.').slice(-2).join('.');
+                    var path = url.pathname;
+                    message = { type: "new_tab", data: { domain: domain, path: path, url: url } };
+                    sendResponse(message);
+                } else {
+                    console.log("invalid-url")
+                }
+
             });
             // Return true to indicate that the sendResponse callback will be called asynchronously.
             return true;
