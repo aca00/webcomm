92d3a33ebc13b824a3200f25681f58dac6b57d4c
aca00 <akhilorappady@gmail.com>
2023-03-04 19:28:41 +0530

    Get URL of current tab - Issue 1: The action.onClicked event will not be dispatched if the extension action has specified a popup to show on click on current tab -- Solution: Use the chrome.tabs.query method to get the active tab's URL. In popup.js file send request to service worker. service-worker will find active tab's url and return to popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,6 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    console.log("Updated");
-    if (tab.url && tab.url.match(new RegExp("http[s]?\:\/\/.*", "gi"))) {
-        console.log("URL match");
-        var url = new URL(tab.url);
-        var domain = url.hostname.split('.').slice(-2).join('.');
-        var path = url.pathname;
-
-        const message = { type: "new_tab", data: { domain: domain, path: path, url: url } };
-        chrome.runtime.sendMessage(message, (response) => {
-            console.log("Received response from service worker:", response);
-            // do something with the response
-        });
-
-        console.log("Message sent")
-    }
+// Send a message to the background script to get the active tab's URL.
+chrome.runtime.sendMessage('getTabUrl', (response) => {
+    // Update the text of a <p> element in the popup to show the URL.
+    const urlText = document.getElementById('para');
+    urlText.textContent = response.url;
 });
--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,20 @@ try {
     importScripts('./dist/bundle.js');
     console.log("Loaded script successfully")
     worker = new Worker.Worker();
-    worker.createNewCollection("a/b/c");
+    // worker.createNewCollection("a/b/c");
+
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message === 'getTabUrl') {
+            // Get the active tab in the current window.
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                // Send the URL to the popup.
+                sendResponse({ url: tabs[0].url });
+            });
+            // Return true to indicate that the sendResponse callback will be called asynchronously.
+            return true;
+        }
+    });
+
 } catch (error) {
     console.error('Failed:', error);
 }
